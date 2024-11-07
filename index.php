2f21a4591c0e489539bf0f3ab5f36fc611987e0f
Gustavo Grancieri <gustavograncieri@outlook.com>
2024-11-06 22:03:27 -0300

    index.php

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tech Solutions - Soluções Tecnológicas</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <header>
+        <h1>Bem-vindo à Tech Solutions</h1>
+        <p>Na Tech Solutions, oferecemos soluções inovadoras para atender às suas necessidades tecnológicas. Fundada em 2024, nossa loja é especializada na venda de produtos eletrônicos de alta qualidade, incluindo laptops, smartphones e tablets. Com um compromisso com a excelência e a inovação, buscamos proporcionar a melhor experiência de compra e um atendimento ao cliente excepcional.</p><br>
+        <nav>
+            <ul>
+                <li><a href="cadastro.php">Cadastro de Clientes</a></li>
+                <li><a href="compra.php">Formulário de Compra</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <img class = "Tablet" src="imagens/-43905672.jpg" alt="">
+    <img class = "Smartphone" src="imagens/-747048218.jpg" alt="">
+    <img class = "Laptop" src="imagens/71AY2Pd3MHL._AC_UF894,1000_QL80_.jpg" alt=""><br>
+<br>
+    <footer>
+        <p>&copy; 2024 Tech Solutions. Todos os direitos reservados.</p>
+    </footer>
+</body>
+</html>
